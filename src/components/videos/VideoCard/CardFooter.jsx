5bd47e433411da9e98cf5a27dd8c2996a1481933
VexoLinx <jvicens4b@gmail.com>
2026-08-02 02:06:00 +0200

    Arreglo en el tamaño de las cards y cambio de variable para recoger source_updated_at en la fecha de la card

--- src/components/videos/VideoCard/CardFooter.jsx
+++ src/components/videos/VideoCard/CardFooter.jsx
@@ -4,10 +4,10 @@ const CardFooter = ({ categoryIcon, categoryName, date, title, userHandle }) => 
       <img src={categoryIcon} alt={categoryName} className="game-icon" />
     </div>
     <div className="card-details">
-      <h3 className="video-title">{title}</h3>
+      <h3 className="video-title" title={title}>{title}</h3>
       <p className="game-name">{categoryName}</p>
       <div className="user-data">
-        <span className="user-handle">{userHandle}</span>
+        <span className="user-handle" title={userHandle}>{userHandle}</span>
         <p className="separador">-</p>
         <p className="upload-date">{date}</p>
       </div>
